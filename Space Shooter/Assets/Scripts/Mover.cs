351e6d6566723f6a18899157624f43e30c0a2fa0
Kaller Long <maoyulong_gz@hotmail.com>
2018-03-15 14:48:05 +0800

    Commit Space Shooter

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float Speed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        rb.velocity = this.transform.forward * Speed;
+        Debug.Log(this.transform.forward.ToString());
+	}
+	
+}
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin;
+    public float xMax;
+    public float zMin;
+    public float zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed;
+    public float tilt;
+    public Boundary boundary;
+
+    private Rigidbody rb;
+    private float nextFire;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = movement * Speed;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, -rb.velocity.x * tilt);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+
+}
